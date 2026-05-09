0c8586950f6cdfb6871581e57505dae5f7bc364e
Shaiv <shaiv@Shaivs-MacBook-Air.local>
2026-05-09 15:40:12 +0530

    refined the xg boost model

--- app.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from dotenv import load_dotenv
-from app.config import port
-
-load_dotenv()
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
